6d12a3623167a81e906cf2d47afd1a71cd6659cd
Khanh Mai <khanhmai@creighton.edu>
2021-05-04 15:49:20 +0000

    lunch break

---
+++ markov.py
@@ -0,0 +1,87 @@
+"""Generate Markov text from text files."""
+
+from random import choice
+
+
+def open_and_read_file(file_path):
+    """Take file path as string; return text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+
+    # your code goes here
+
+    contents = open(file_path).read() # open and read file
+
+    words = contents.split() # seperate by whitespace
+
+    print(contents)
+
+    return contents #'Contents of your file as one long string '
+    
+
+def make_chains(text_string):
+    """Take input text as string; return dictionary of Markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains('hi there mary hi there juanita')
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+
+        >>> chains[('there','juanita')]
+        [None]
+    """
+    chains = {}
+
+    # words = text_string.split() #split string
+
+    # loop over every two word pair in the text
+    for i in range(len(text_string) - 2):
+        key = (text_string[i], text_string[i + 1])
+        chains[key] = []
+        chains[key].append(text_string[i + 2])
+        print(text_string[i], text_string[i + 1])
+
+
+    # your code goes here
+
+    return chains
+    print(chains)
+
+
+def make_text(chains):
+    """Return text from chains."""
+
+    words = []
+
+    # your code goes here
+
+    return ' '.join(words)
+
+
+input_path = 'green-eggs.txt'
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+print(random_text)
